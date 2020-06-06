a3b1442d2abb61e08ddc2f1e0b6701e26e0a35c8
Janis Lebron <janislebron@gmail.com>
2020-06-06 20:45:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,16 @@ attr_accessor :name
 
   def initialize(name)
     @name = name
-    @@all << self
   end
 
   def self.all
     @@all.each do |dog|
       puts dog.name
     end
+  end
+
+  def save
+    @@all << self
   end
 
   def self.clear_all
