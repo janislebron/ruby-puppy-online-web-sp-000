bf03cbc5dba56f8498d6635b0bf98a93e8f81867
Janis Lebron <janislebron@gmail.com>
2020-06-06 20:46:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,11 +9,14 @@ attr_accessor :name
   end
 
   def self.all
+    @@all
+  end
+
+  def self.print_all
     @@all.each do |dog|
       puts dog.name
     end
   end
-
   def save
     @@all << self
   end
